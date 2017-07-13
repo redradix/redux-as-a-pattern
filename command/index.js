6aa7f83afc984fdbf3a19f9d78cbb16b2382e0e0
Victor <victor.miguez.martinez@gmail.com>
2017-07-13 20:54:38 +0200

    Matrix paja mental

--- command/index.js
+++ command/index.js
@@ -1,38 +1,115 @@
+
 'use strict'
+
 var printMatrix = require('./printMatrix')
 
 function Matrix(width, height, initialPosition) {
 	var position = initialPosition || [0, 0]
+	var commands = []
+	var _callback
 
 	return {
-		moveRight: function() {
-			position[0]++
+		dispatch: function(action) {
+			commands.push(action)
+			var data = action.execute({
+				position: position,
+				width: width,
+				height: height
+			})
+			position = data.position
+			width = data.width
+			_callback(width, height, position)
 		},
-		// move: function(movement) {
-		// 	position = movement.execute(position)
-		// },
-		print: function() {
-			printMatrix(width, height, position)
+		undo: function() {
+			var action = commands.pop()
+			var data = action.undo()
+			position = data.position
+			width = data.width
+			_callback(width, height, position)
+		},
+		onChange: function(callback) {
+			_callback = callback
 		}
 	}
 }
 
-function Right() {
-	return {
-		execute: function(position) {
-			return [position[0] + 1, position[1]]
+function Command() {
+	this.prev
+	this.undo = function() {
+		return this.prev
+	}
+}
+
+function Right(n) {
+	this.execute =function(data) {
+		this.prev = data
+		return {
+			position: [data.position[0] + n, data.position[1]],
+			width: data.width,
+			height: data.height
 		}
 	}
 }
 
+Right.prototype = new Command()
+
+function Left() {
+	this.execute = function(data) {
+		this.prev = data
+		return {
+			position: [data.position[0] - 1, data.position[1]],
+			width: data.width,
+			height: data.height
+		}
+	}
+}
+
+Left.prototype = new Command()
+
+function Down() {
+	this.execute = function(data) {
+		this.prev = data
+		return {
+			position: [data.position[0], data.position[1] + 1],
+			width: data.width,
+			height: data.height
+		}
+	}
+}
+
+Down.prototype = new Command()
+
+function SetWidth(width) {
+	this.execute = function(data) {
+		this.prev = data
+		return {
+			position: data.position,
+			width: width,
+			height: data.height
+		}
+	}
+}
+
+SetWidth.prototype = new Command()
 
 var WIDTH = 4
 var HEIGHT = 4
 
 var matrix = Matrix(WIDTH, HEIGHT)
+matrix.onChange(printMatrix)
 
-matrix.print()
-matrix.moveRight()
-// matrix.move(new Right())
-matrix.print()
+matrix.dispatch(new Right(2))
 
+matrix.undo()
+
+matrix.dispatch(new Right(1))
+
+matrix.dispatch(new Down())
+
+matrix.dispatch(new Left())
+
+matrix.undo()
+
+matrix.dispatch(new SetWidth(5))
+
+matrix.undo()
